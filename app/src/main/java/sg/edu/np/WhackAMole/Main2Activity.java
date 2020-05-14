52691d2228ef84da5c58d8658ffcad067476b600
Yoon Teng Lee <s10198259@connect.np.edu.sg>
2020-05-14 21:25:52 +0800

    Completed Practical 4

--- app/src/main/java/sg/edu/np/WhackAMole/Main2Activity.java
+++ app/src/main/java/sg/edu/np/WhackAMole/Main2Activity.java
@@ -20,7 +20,15 @@ public class Main2Activity extends AppCompatActivity {
         - Feel free to modify the function to suit your program.
     */
 
+    private Button Button1, Button2, Button3, Button4, Button5, Button6, Button7, Button8, Button9;
+    TextView advancedScore;
+    private Integer score;
+    private String b1, b2, b3, b4, b5, b6, b7, b8, b9;
 
+    CountDownTimer myCountDown;
+    CountDownTimer readyTimer;
+
+    final String TAG = "Whack-A-Mole 2.0!";
 
     private void readyTimer(){
         /*  HINT:
@@ -32,6 +40,23 @@ public class Main2Activity extends AppCompatActivity {
             belongs here.
             This timer countdown from 10 seconds to 0 seconds and stops after "GO!" is shown.
          */
+        readyTimer = new CountDownTimer(11000, 2000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.v(TAG, "Ready Countdown!" + millisUntilFinished/1000);
+                Toast.makeText(getApplicationContext(), "Get Ready in " + millisUntilFinished/1000 + "seconds", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onFinish() {
+                Log.v(TAG, "Countdown Complete!");
+                Toast.makeText(getApplicationContext(), "GO!", Toast.LENGTH_SHORT).show();
+                readyTimer.cancel();
+                placeMoleTimer();
+            }
+        };
+        readyTimer.start();
     }
     private void placeMoleTimer(){
         /* HINT:
@@ -41,6 +66,7 @@ public class Main2Activity extends AppCompatActivity {
            belongs here.
            This is an infinite countdown timer.
          */
+        countDownTimer();
     }
     private static final int[] BUTTON_IDS = {
         /* HINT:
@@ -59,8 +85,22 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Log.v(TAG, "Current User Score: " + String.valueOf(advancedScore));
+        Intent recData = getIntent();
+        score = recData.getIntExtra("Score", 0);
+        advancedScore = (TextView) findViewById(R.id.advScore);
+        advancedScore.setText(""+score);
 
+        Log.v(TAG, "Current User Score: " + String.valueOf(score));
+
+        Button1 = (Button) findViewById(R.id.button1);
+        Button2 = (Button) findViewById(R.id.button2);
+        Button3 = (Button) findViewById(R.id.button3);
+        Button4 = (Button) findViewById(R.id.button4);
+        Button5 = (Button) findViewById(R.id.button5);
+        Button6 = (Button) findViewById(R.id.button6);
+        Button7 = (Button) findViewById(R.id.button7);
+        Button8 = (Button) findViewById(R.id.button8);
+        Button9 = (Button) findViewById(R.id.button9);
 
         for(final int id : BUTTON_IDS){
             /*  HINT:
@@ -68,12 +108,25 @@ public class Main2Activity extends AppCompatActivity {
             You may use if you wish to remove or change to suit your codes.
             */
         }
+
+        readyTimer();
+        doCheck();
     }
     @Override
     protected void onStart(){
         super.onStart();
+
+        b1 = Button1.getText().toString();
+        b2 = Button2.getText().toString();
+        b3 = Button3.getText().toString();
+        b4 = Button4.getText().toString();
+        b5 = Button5.getText().toString();
+        b6 = Button6.getText().toString();
+        b7 = Button7.getText().toString();
+        b8 = Button8.getText().toString();
+        b9 = Button9.getText().toString();
     }
-    private void doCheck(Button checkButton)
+    private void doCheck()
     {
         /* Hint:
             Checks for hit or miss
@@ -81,6 +134,150 @@ public class Main2Activity extends AppCompatActivity {
             Log.v(TAG, "Missed, point deducted!");
             belongs here.
         */
+        Button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b1 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b2 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button3.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b3 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button4.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b4 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button5.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b5 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button6.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b6 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button7.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b7 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button8.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b8 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
+        Button9.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                onStart();
+                if(b9 == "*"){
+                    score += 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    score -= 1;
+                    advancedScore.setText(""+score);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+            }
+        });
     }
 
     public void setNewMole()
@@ -91,6 +288,123 @@ public class Main2Activity extends AppCompatActivity {
          */
         Random ran = new Random();
         int randomLocation = ran.nextInt(9);
+
+        if(randomLocation == 0) {
+            Button1.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 1){
+            Button2.setText("*");
+            Button1.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 2){
+            Button3.setText("*");
+            Button2.setText("O");
+            Button1.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 3){
+            Button4.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button1.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 4){
+            Button5.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button1.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 5){
+            Button6.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button1.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 6){
+            Button7.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button1.setText("O");
+            Button8.setText("O");
+            Button9.setText("O");
+        }
+        else if (randomLocation == 7){
+            Button8.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button1.setText("O");
+            Button9.setText("O");
+        }
+        else{
+            Button9.setText("*");
+            Button2.setText("O");
+            Button3.setText("O");
+            Button4.setText("O");
+            Button5.setText("O");
+            Button6.setText("O");
+            Button7.setText("O");
+            Button8.setText("O");
+            Button1.setText("O");
+        }
+    }
+
+    private void countDownTimer() {
+
+        myCountDown = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                setNewMole();
+                Log.v(TAG, "New Mole Location!");
+            }
+
+            @Override
+            public void onFinish() {
+                myCountDown.start();
+            }
+        };
+        myCountDown.start();
     }
 }
 
--- app/src/main/java/sg/edu/np/WhackAMole/MainActivity.java
+++ app/src/main/java/sg/edu/np/WhackAMole/MainActivity.java
@@ -1,8 +1,8 @@
 package sg.edu.np.WhackAMole;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +23,15 @@ public class MainActivity extends AppCompatActivity {
         - Feel free to modify the function to suit your program.
     */
 
+    private Button ButtonLeft;
+    private Button ButtonMiddle;
+    private Button ButtonRight;
+    private TextView Score;
+    private String left, middle, right;
+    private Integer point = 1;
+
+    final String TAG = "Whack-A-Mole 1.0!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,20 +39,28 @@ public class MainActivity extends AppCompatActivity {
 
         Log.v(TAG, "Finished Pre-Initialisation!");
 
+        ButtonLeft = (Button) findViewById(R.id.ButtonLeft);
+        ButtonMiddle = (Button) findViewById(R.id.ButtonMiddle);
+        ButtonRight = (Button) findViewById(R.id.ButtonRight);
+        Score = (TextView) findViewById(R.id.Score);
 
+        doCheck();
     }
     @Override
     protected void onStart(){
         super.onStart();
         setNewMole();
         Log.v(TAG, "Starting GUI!");
+
+        left = ButtonLeft.getText().toString();
+        middle = ButtonMiddle.getText().toString();
+        right = ButtonRight.getText().toString();
     }
     @Override
     protected void onPause(){
         super.onPause();
         Log.v(TAG, "Paused Whack-A-Mole!");
     }
-
     @Override
     protected void onStop(){
         super.onStop();
@@ -51,10 +68,76 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    private void doCheck(Button checkButton) {
+    private void doCheck() {
         /* Checks for hit or miss and if user qualify for advanced page.
             Triggers nextLevelQuery().
          */
+        point = point - 1;
+
+        ButtonLeft.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                Log.v(TAG, "Button Left Clicked!");
+                if(left == "*"){
+                    point += 1;
+                    Score.setText(""+point);
+                    if(point%10 == 0){
+                        nextLevelQuery();
+                    }
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    point -= 1;
+                    Score.setText(""+point);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+                onStart();
+            }
+        });
+
+        ButtonMiddle.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+
+                Log.v(TAG, "Button Middle Clicked!");
+                if(middle == "*"){
+                    point += 1;
+                    Score.setText(""+point);
+                    if(point%10 == 0){
+                        nextLevelQuery();
+                    }
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    point -= 1;
+                    Score.setText(""+point);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+                onStart();
+            }
+        });
+
+        ButtonRight.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+
+                Log.v(TAG, "Button Right Clicked!");
+                if(right == "*"){
+                    point += 1;
+                    Score.setText(""+point);
+                    if(point%10 == 0){
+                        nextLevelQuery();
+                    }
+                    Log.v(TAG, "Hit, score added!");
+                }
+                else{
+                    point -= 1;
+                    Score.setText(""+point);
+                    Log.v(TAG, "Missed, score deducted!");
+                }
+                onStart();
+            }
+        });
     }
 
     private void nextLevelQuery(){
@@ -64,14 +147,53 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "User decline!");
         Log.v(TAG, "Advance option given to user!");
         belongs here*/
+        Log.v(TAG, "Advance option given to user!");
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Would you like to advance to advanced mode?").setCancelable(false);
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Log.v(TAG, "User accepts!");
+                nextLevel();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Log.v(TAG, "User declines!");
+                onResume();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.setTitle("Warning! Insane Whack-A-Mole Incoming!");
+        alert.show();
     }
 
     private void nextLevel(){
         /* Launch advanced page */
+        Intent nextLevel = new Intent(MainActivity.this, Main2Activity.class);
+        nextLevel.putExtra("Score", point);
+        startActivity(nextLevel);
     }
 
     private void setNewMole() {
         Random ran = new Random();
         int randomLocation = ran.nextInt(3);
+
+        if(randomLocation == 0) {
+            ButtonLeft.setText("*");
+            ButtonMiddle.setText("O");
+            ButtonRight.setText("O");
+        }
+        else if (randomLocation == 1){
+            ButtonLeft.setText("O");
+            ButtonMiddle.setText("*");
+            ButtonRight.setText("O");
+        }
+        else{
+            ButtonLeft.setText("O");
+            ButtonMiddle.setText("O");
+            ButtonRight.setText("*");
+        }
     }
 }
